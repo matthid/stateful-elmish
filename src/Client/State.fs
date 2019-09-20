b11cc8c3b6a769406cadf75bd17a7139cd2b9ed5
Matthias Dittrich <matthi.d@gmail.com>
2019-09-20 18:23:24 +0200

    initial start

---
+++ src/Client/State.fs
@@ -0,0 +1,20 @@
+
+module State
+
+open Elmish
+
+// This model 'needs to be updated' from multiple components
+type Model = { State: int }
+
+// The Msg type defines what events/actions can occur while the application is running
+// the state of the application changes *only* in reaction to these events
+type Msg =
+    | Increment
+    | Decrement
+
+let init () =
+    { State = 0 }
+
+let update (msg:Msg) (model:Model) =
+    model, Cmd.none
+
